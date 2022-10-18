aff5612c3470bcda6e1a86c9529101141965b85e
irshadyusuf <irshady647@gmail.com>
2022-10-18 16:13:30 +0530

    photo

--- mech_hub/settings.py
+++ mech_hub/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-xd!xaxaiq_d6)f6yo^8*=gl%qt6^ilf8fwy$#zhy0kn+6iur9=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'mech_hub.urls'
